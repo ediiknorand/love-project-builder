499df4106b6c4b1af75811c65a4a9478c17532f3
ediiknorand <ediiknorand@users.noreply.github.com>
2017-11-19 23:19:30 -0200

    fix main.mouse.lua template

--- template/main.mouse.lua
+++ template/main.mouse.lua
@@ -1,14 +1,14 @@
 function love.mousemoved(x, y, dx, dy)
   local w,h = love.graphics.getDimensions()
-  State.getCurrentState():mousemoved(x*vWidth/w, y*vHeight/h, dx, dy)
+  State.get_current():mousemoved(x*vWidth/w, y*vHeight/h, dx, dy)
 end
 
 function love.mousepressed(x, y, button)
   local w,h = love.graphics.getDimensions()
-  State.getCurrentState():mousepressed(x*vWidth/w, y*vHeight/h, button)
+  State.get_current():mousepressed(x*vWidth/w, y*vHeight/h, button)
 end
 
 function love.mousereleased(x, y, button)
   local w,h = love.graphics.getDimensions()
-  State.getCurrentState():mousereleased(x*vWidth/w, y*vHeight/h, button)
+  State.get_current():mousereleased(x*vWidth/w, y*vHeight/h, button)
 end
